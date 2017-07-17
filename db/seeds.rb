2af8fa8107d2d660f0b0e5c7f67caa0ccdea3f4c
stevenmills <samills0608@gmail.com>
2017-07-17 17:52:39 -0400

    blocipedia-4-CRUD: Created Seeds and worked on Models and Controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,15 @@
+require 'random_data'
 
+50.times do
+  Wiki.create!(
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    private: false
+  )
+end
+
+wikis = Wiki.all
+
+
+puts "Seed Finished"
+puts "#{Wiki.count} wikis created"
--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Wiki, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:wiki) { Wiki.create!(title: "New Wiki Title", body: "New Wiki Body", private: false) }
+
+  describe "attributes" do
+    it "has title, body and private attributes" do
+      expect(wiki).to have_attributes(title: "New Wiki Title", body: "New Wiki Body", private: false)
+    end
+  end
 end
