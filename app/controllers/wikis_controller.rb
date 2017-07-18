72d51941fe8fdf86e3769b2b8d53d1298caef4ac
stevenmills <samills0608@gmail.com>
2017-07-18 16:35:50 -0400

    blocipedia-4-CRUD: Added the new and create specs

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -7,6 +7,7 @@ class WikisController < ApplicationController
   end
 
   def new
+    @wiki = Wiki.all
   end
 
   def edit
--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -23,12 +23,39 @@ RSpec.describe WikisController, type: :controller do
 #    end
 #  end
 
-#  describe "GET #new" do
-#    it "returns http success" do
-#      get :new
-#      expect(response).to have_http_status(:success)
-#    end
-#  end
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #new view" do
+      get :new
+      expect(response).to render_template :new
+    end
+
+    it "instantiates @wiki" do
+      get :new
+      expect(assigns(:wiki)).not_to be_nil
+    end
+  end
+
+  describe "POST create" do
+
+    it "increases the number of Wiki by 1" do
+      expect{wiki :create, wiki: {title: RandomData.random_sentence, body: RandomData.random_paragraph, private: false}}.to change(Wiki,:count).by(1)
+    end
+
+    it "assigns the new wiki to @wiki" do
+      wiki :create, wiki: {title: RandomData.random_sentence, body: RandomData.random_paragraph, private: false}
+      expect(assigns(:wiki)).to eq Wiki.last
+    end
+
+    it "redirects to the new wiki" do
+      wiki :create, wiki: {title: RandomData.random_sentence, body: RandomData.random_paragraph, private: false}
+      expect(response).to redirect_to Wiki.last
+    end
+  end
 
 #  describe "GET #edit" do
 #    it "returns http success" do
